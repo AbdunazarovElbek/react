adf427f6818753ddb83a0173f1ddbebbb061439e
AbdunazarovElbek <abdunazarovelbek.09@gmail.com>
2025-01-04 08:53:29 +0500

    rect proekt

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+
+// import './index.css'
+// import { useState } from 'react' 
+
+// import { useState } from "react";
+
+
+
+// function App(){
+//   let [count , setCount] = useState(0)
+  
+//   return(
+//       <>
+//       <h1>{count}</h1>
+//       <button onClick={() =>setCount(count += 1)} >+</button>
+//       <button onClick={() =>setCount(count -= 1)} >-</button>
+//       <button onClick={() =>setCount(count = 0)} >reset</button>
+//       </>
+//   )
+//   }
+
+// export default App
+
+// function App(){
+//   let arr = ["olam" , "banan" , "nok"]
+  
+//   return(
+     
+//     arr.map((item , index) => {
+//       return(
+//         <h1 key={index}>{item}</h1>
+//       )
+//     })
+//   )
+//   }
+
+// export default App
+
+
+// function App(){
+
+//   const [active, setActive] = useState(false)
+//   const [activ, setActiv] = useState(false)
+//   return(
+//     <>
+
+//     <button
+//     style={active ? {backgroundColor:"red"} : {backgroundColor : "white"}}
+//     onClick={() => {setActive(!active); setActiv(false)}}
+//     >👍</button>
+    
+//      <button
+//     style={activ ? {backgroundColor:"black"} : {backgroundColor : "white"}}
+//     onClick={() => {setActiv(!active) ;setActive(false)}}
+//     >👎</button>
+
+//     </>
+//   )
+// }
+// export default App
+
+
+import './index.css'
+import'./assets/hero image.png'
+function App(){
+return(
+  <div className='body'>
+
+  </div>
+)
+}
+
+export default App
